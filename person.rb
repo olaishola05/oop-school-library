7fb0c445425fcb7de93800a4cced57ac4d2b0506
Ola Ishola <olaishola@hotmail.co.uk>
2022-04-04 21:05:23 +0100

    Add files for students & teachers, extend Person class

--- person.rb
+++ person.rb
@@ -1,32 +1,24 @@
 class Person
   attr_accessor :name, :age, :id
 
-  def initialize(id, name = "Unknown", age, parent_permission)
+  def initialize(age, parent_permission: true, name: 'Unknown')
     @id = id
     @name = name
     @age = age
-    @parent_permission = true
+    @parent_permission = parent_permission
   end
 
   def can_use_services?
-    if age >= 18 && parent_permission == true
-      true
-    end
+    true if age >= 18 && @parent_permission
   end
-
 
   private
 
-  def is_of_age?
-    if age >= 18 true
-      false
-
+  def of_age?
+    true if age >= 18
+    false
   end
-
 end
- 
 
-testObj = Person.new(1, "John", 18, true)
-puts testObj.can_use_services?
-
-
+ola = Person.new(17)
+puts ola.can_use_services?
