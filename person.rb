21082c022fdae0fef3d60029390a158fa53d7e9d
Ola Ishola <olaishola@hotmail.co.uk>
2022-04-05 14:16:24 +0100

    Fix linters errors

--- person.rb
+++ person.rb
@@ -28,9 +28,9 @@ class Person < Nameable
   end
 end
 
-ola = Person.new(17, 'maximilianus')
-puts ola.correct_name
-capitalizedPerson = CapitalizeDecorator.new(ola)
-puts capitalizedPerson.correct_name
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-puts capitalizedTrimmedPerson.correct_name
+person = Person.new(17, 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
