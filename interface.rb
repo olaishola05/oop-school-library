8523a9acd8a47771dee05acb4f18599c1358489d
Ola Ishola <olaishola@hotmail.co.uk>
2022-04-12 02:42:52 +0100

    Refactor basedecorator to be on its own file

--- interface.rb
+++ interface.rb
@@ -4,18 +4,6 @@ class Nameable
   end
 end
 
-class BaseDecorator < Nameable
-  attr_accessor :nameable
-
-  def initialize(nameable)
-    super()
-    @nameable = nameable
-  end
-
-  def correct_name
-    @nameable.correct_name
-  end
-end
 
 class CapitalizeDecorator < BaseDecorator
   def correct_name
